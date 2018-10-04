5edf72462e34219fa27a1187df8cf2d749032fd9
acuteangel <akaquas@gmail.com>
2018-10-03 18:46:00 -0700

    added missing code

--- logicOption2.js
+++ logicOption2.js
@@ -8,9 +8,9 @@ var config = {
     messagingSenderId: "1033666292141"
   };
   
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
   
-  var database = firebase.database();
+var database = firebase.database();
 
 // Initial Values
 var initialBid = 0;
@@ -36,7 +36,7 @@ highBidder = snapshot.val().highBidder;
 $("#highest-bidder").text(highBidder);
 $("#highest-price").text(highPrice);
 // Print the data to the console.
-
+  console.log(snapshot.val())
 
 // Else Firebase doesn't have a highPrice/highBidder, so use the initial local values.
 
@@ -45,37 +45,47 @@ $("#highest-price").text(highPrice);
 
 
 // Print the data to the console.
-
+});
 
 
 
 // --------------------------------------------------------------
 
 // Whenever a user clicks the submit-bid button
-
+$("#submit-bid").on("click", function(event) {
 // prevent form from submitting with event.preventDefault() or returning false
-
+  event.preventDefault();
 // Get the input values
-
-
+  var newPrice = $("#bidder-price").val();
+  var newName = $("#bidder-name").val();
 // Log the Bidder and Price (Even if not the highest)
-
+console.log(newPrice + " "+newName);
 
 // If Then statements to compare against previous high bidder
-
+if (newPrice > highPrice) {
 
 // Alert that they are High Bidder
-
+alert("You are the High Bidder");
 
 // Save the new price in Firebase
-
+database.ref().set({
+  highPrice: newPrice,
+  highBidder: newName
+})
 
 // Log the new High Price
-
+console.log(newPrice)
 
 // Store the new high price and bidder name as a local variable (could have also used the firebase variable)
-
+highPrice = newPrice;
+highBidder = newName;
 
 // Change the HTML to reflect the new high price and bidder
 
+$("#highest-bidder").text(highBidder);
+$("#highest-price").text(highPrice);
+}
 // Else tell user their bid was too low via alert
+else {
+  alert("Your bid is too low")
+}});
